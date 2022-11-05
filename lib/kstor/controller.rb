a109441385823f3a6a6e37b482d6550ed9f0df3b
Jérémie Pierson <jeremie.pierson@arlol.net>
2022-11-05 21:44:42 +0100

    Put Controller::RequestHandler in its own file.

--- lib/kstor/controller.rb
+++ lib/kstor/controller.rb
@@ -7,6 +7,7 @@ require 'kstor/message'
 require 'kstor/controller/authentication'
 require 'kstor/controller/secret'
 require 'kstor/controller/users'
+require 'kstor/controller/request-handler'
 
 module KStor
   # Error: user was not allowed to access application.
@@ -36,45 +37,5 @@ module KStor
   class MissingArgument < Error
     error_code 'REQ/MISSINGARG'
     error_message 'Missing argument %s for request type %s'
-  end
-
-  module Controller
-    # Request handler.
-    class RequestHandler
-      def initialize(store, session_store)
-        @auth = Controller::Authentication.new(store, session_store)
-        @secret = Controller::Secret.new(store)
-        @user = Controller::User.new(store)
-        @store = store
-      end
-
-      def handle_request(req)
-        user, sid = @auth.authenticate(req)
-        controller = controller_from_request_type(req)
-        resp = @store.transaction { controller.handle_request(user, req) }
-        user.lock
-        resp.session_id = sid
-        resp
-      rescue RbNaClError => e
-        Log.exception(e)
-        Error.for_code('CRYPTO/UNSPECIFIED').response
-      rescue Error => e
-        Log.info(e.message)
-        e.response
-      end
-
-      private
-
-      def controller_from_request_type(req)
-        case req.type
-        when /^secret-(create|delete|search|unlock|update-(meta|value)?)$/
-          @secret
-        when /^group-create$/
-          @user
-        else
-          raise Error.for_code('REQ/UNKNOWN', req.type)
-        end
-      end
-    end
   end
 end
---
+++ lib/kstor/controller/request-handler.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module KStor
+  module Controller
+    # Request handler.
+    class RequestHandler
+      def initialize(store, session_store)
+        @auth = Controller::Authentication.new(store, session_store)
+        @secret = Controller::Secret.new(store)
+        @user = Controller::User.new(store)
+        @store = store
+      end
+
+      def handle_request(req)
+        user, sid = @auth.authenticate(req)
+        controller = controller_from_request_type(req)
+        resp = @store.transaction { controller.handle_request(user, req) }
+        user.lock
+        resp.session_id = sid
+        resp
+      rescue RbNaClError => e
+        Log.exception(e)
+        Error.for_code('CRYPTO/UNSPECIFIED').response
+      rescue Error => e
+        Log.info(e.message)
+        e.response
+      end
+
+      private
+
+      def controller_from_request_type(req)
+        case req.type
+        when /^secret-(create|delete|search|unlock|update-(meta|value)?)$/
+          @secret
+        when /^group-create$/
+          @user
+        else
+          raise Error.for_code('REQ/UNKNOWN', req.type)
+        end
+      end
+    end
+  end
+end
