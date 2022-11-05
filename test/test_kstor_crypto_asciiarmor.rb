abb42748cc3ac1b8ca48a72a1b4cdc8b61651f58
Jérémie Pierson <jeremie.pierson@arlol.net>
2022-11-05 22:21:11 +0100

    Add tests for KStor::Crypto::ASCIIArmor

---
+++ test/test_kstor_crypto_asciiarmor.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class TestKStorCryptoASCIIArmor < Minitest::Test
+  def test_roundtrip
+    s = 'çui-ci est en UTF-8 héhéhé'.dup.force_encoding('ASCII-8BIT')
+    roundtrip = KStor::Crypto::ASCIIArmor.decode(
+      KStor::Crypto::ASCIIArmor.encode(s)
+    )
+    assert_equal(s, roundtrip)
+  end
+end
